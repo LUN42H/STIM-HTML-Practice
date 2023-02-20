fcc86cf787edab052dc0141b10e04fa3fd73435a
LUN42H <120771982+LUN42H@users.noreply.github.com>
2023-02-19 18:38:54 -0800

    Update stim_task.js

--- stim_task.js
+++ stim_task.js
@@ -1,28 +1,50 @@
-var media     = document.getElementById('vid');
-var labelNew  = document.getElementById('labelSelected').value;
+const STIM_PATH = 'stim/27movies/';
+const VID_LIST = [
+    '1012_push.mp4', '1145_leave.mp4', '4397_hug.mp4', '4408_lead.mp4', '5407_kiss.mp4', 
+    '5787_accompany.mp4', '5809_pull.mp4', '5814_talk to.mp4', '5816_ignore.mp4', '5843_huddle with.mp4', 
+    '5847_approach.mp4', '5849_approach.mp4', '5866_follow.mp4', '5870_poke.mp4', '5871_pull.mp4', 
+    '5875_escape.mp4', '5878_follow.mp4', '5902_hit.mp4', '5905_encircle.mp4', '5908_creep up on.mp4', 
+    '5914_tickle.mp4', '5929_leave.mp4', '5930_accompany.mp4', '5943_scratch.mp4', '5948_flirt.mp4', 
+    '5945_flight.mp4', '5986_fight.mp4', '5987_creep up on.mp4', '5989_huddle with.mp4', '5991_examine.mp4', 
+    '5998_tickle.mp4', '6002_throw.mp4', '6004_herd.mp4', '6005_throw.mp4', '6012_kiss.mp4', 
+    '6016_scratch.mp4', '6079_bother.mp4', '6035_capture.mp4', '6034_avoid.mp4', '6023_hit.mp4', 
+    '6017_encircle.mp4'
+];
+const TRIAL_LIST = shuffle_array(VID_LIST);
+
+var media     = document.querySelector('#vid');
 var labelList = [];
 var index     = 0
 var duration  = media.duration;
+var selectedItem;
 
 // button functions
 function clickReplay() {
-    alert(media.currentTime);
+    // alert(1);
     media.currentTime = 0;
-    //media.onload();
-    //media.play();
-}
+    media.play();
+} 
 
 function clickInstruction() {
     alert("Please select a label that best describes the animation. Proceed to the next section by clicking Next.");
 }
 
 function clickNext() {
-    labelList.push(labelNew);
+    var selectedItem = document.querySelector('#labelSelected').value;
+    if (selectedItem == "select") { // select a value before proceeding
+        alert("Please select a label.");
+        return;
+    }
+
+    labelList.push(selectedItem);
     index ++;
+
     if (index >= 27) {
        this.end();
     }
-    this.showNew();
+    else {
+        this.showNew(); 
+    }
 } 
 
 // display new video
